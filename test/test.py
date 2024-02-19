3c68ac138ba02f9193881ad9c0f2ea98bf9aa70f
kyoungbin <skjin20800@naver.com>
2024-02-19 19:08:12 +0900

    전화번호 목록

--- test/test.py
+++ test/test.py
@@ -1,9 +1,20 @@
-def solution(A,B):
-    A.sort(reverse=True)
-    B.sort()
+def solution(phone_book):
+    phone_book.sort(key=len)   
+    phone_dict = {}
+    
+    
+    
+    for phone_number in phone_book:
+        phone_dict[phone_number] = 1
         
-    value = 0
-    for pair1,pair2 in zip(A, B):
-        value = value + (pair1*pair2)
+    for phone_number in phone_book:
+        temp = ""
+        
+        for number in phone_number:
+            temp += number
+            
+            if temp in phone_dict and temp != phone_number:           
+                return False                                                                    
+    return True   
 
-    return value
+solution(["119", "97674223", "1195524421"])
