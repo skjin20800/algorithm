853c1fdce4a7f15db5917a078d13a1a554523bb0
kyoungbin <skjin20800@naver.com>
2024-02-29 16:28:42 +0900

    DFS 네트워크

--- test/test.py
+++ test/test.py
@@ -1,17 +1,24 @@
-result_list = []
-def dfs(current_number,numbers, target):
+def dfs(root_index, computer_index, computers, visited):
+    if visited[computer_index]:  # 이미 방문한 컴퓨터인 경우 종료
+        return 0  # 네트워크 개수 증가 없음
+    visited[computer_index] = True  # 현재 컴퓨터를 방문으로 표시
+        
+    network_list = computers[computer_index]
+    count = 0  # 네트워크 개수를 세는 변수
+    for index, value in enumerate(network_list):
+        if value == 1:  # 연결된 컴퓨터가 있는 경우 재귀적으로 탐색
+            count += dfs(root_index, index, computers, visited)
     
-    if len(numbers) >= 1 :
-        plus_number = current_number + numbers[0]
-        minus_number = current_number - numbers[0]
-        dfs(plus_number,numbers[1:],target) 
-        dfs(minus_number,numbers[1:],target) 
-    else :
-        if current_number == target:
-            result_list.append(current_number)
-            
-def solution(numbers, target):
-    numbers.insert(0, 0)
-    dfs(numbers[0],numbers[1:],target)      
+    if root_index == computer_index:  # 시작 컴퓨터와 현재 컴퓨터가 같은 경우
+        return count + 1  # 네트워크 개수를 1 증가시킴
+    else:
+        return count  # 네트워크 개수 증가 없음
+
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n  # 방문 여부를 저장하는 리스트
     
-    return len(result_list)
+    for index in range(n):
+        answer += dfs(index, index, computers, visited)  # 각 컴퓨터를 시작점으로 DFS 탐색
+    
+    return answer
