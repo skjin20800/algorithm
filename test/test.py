c84c67cb39a1535c93a0ade96533977c1688f2a4
kyoungbin <skjin20800@naver.com>
2024-02-29 15:26:21 +0900

    DFS 타겟 넘버

--- test/test.py
+++ test/test.py
@@ -1,4 +1,17 @@
-def solution(num1, num2):
-    answer = 0
+result_list = []
+def dfs(current_number,numbers, target):
     
-    return num1 - num2
+    if len(numbers) >= 1 :
+        plus_number = current_number + numbers[0]
+        minus_number = current_number - numbers[0]
+        dfs(plus_number,numbers[1:],target) 
+        dfs(minus_number,numbers[1:],target) 
+    else :
+        if current_number == target:
+            result_list.append(current_number)
+            
+def solution(numbers, target):
+    numbers.insert(0, 0)
+    dfs(numbers[0],numbers[1:],target)      
+    
+    return len(result_list)
