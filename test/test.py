ab25397c03f7a7c35d7cc0fae985c9ee23e439a4
kyoungbin <skjin20800@naver.com>
2024-03-04 18:09:27 +0900

    BFS 단어 변환

--- test/test.py
+++ test/test.py
@@ -1,80 +1,58 @@
 from queue import Queue
-import copy
 
-answer = []
 
-# Current 클래스 정의
-class Character:
-    def __init__(self, x, y, count, maps):
-        self.x = x
-        self.y = y
+queue = Queue()
+
+class Tansform:
+    def __init__(self, count,word, words):    
         self.count = count
-        self.maps = maps
-        
-    def get_x(self):
-        return self.x
+        self.word = word
+        self.words = words
+    def get_item(self):
+        return self
     
-    def get_y(self):
-        return self.y
-    
-    def get_count(self):
-        return self.count
-    
-    def get_maps(self):
-        return self.maps
 
-def bfs(max_x, max_y, maps):
-    queue = Queue()  # 큐 초기화
+def bfs(begin, target,words):
     
-    init_character = Character(0, 0, 0, maps)
-    queue.put(init_character)
+    init_transform = Tansform(0, begin, words)
+    queue.put(init_transform)   
     
-    # 큐에 항목이 남아있는 동안 루프 실행
-    while not queue.empty():
+    value = 0
+    
+    while not queue.empty():        
         item = queue.get()
-        cur_x = item.get_x()    
-        cur_y = item.get_y()
-        count = item.get_count()
-        maps = item.get_maps()
-        
-        # 사각형 밖으로 나갔을 때
-        if cur_x < 0 or cur_y < 0 or cur_x >= max_x or cur_y >= max_y:
-            continue
-        # 벽으로 막혀있을 때
-        if maps[cur_y][cur_x] == 0:
-            continue
-        # 이미 최단거리 구했을 때
-        if len(answer) > 0 and count >= min(answer):
-            continue
-        
-        # 현재 장소를 벽으로 막고 거리 카운트 +1 
-        maps[cur_y][cur_x] = 0
-        count = count + 1
-        
-        # 도착 지점이면 최단거리 저장
-        if cur_x == max_x - 1 and cur_y == max_y - 1:
-            answer.append(count)       
-        
-        # 4가지 방향으로 이동
-        character1 = Character(cur_x + 1, cur_y, count, maps)
-        queue.put(character1)
-        
-        character2 = Character(cur_x - 1, cur_y, count, maps)
-        queue.put(character2)
-        
-        character3 = Character(cur_x, cur_y + 1, count, maps)
-        queue.put(character3)
-        
-        character4 = Character(cur_x, cur_y - 1, count, maps)
-        queue.put(character4)
+        cur_word = item.word
+        words = item.words
+        cur_count = item.count
+   
+        # 타겟 검사
+        if cur_word == target:
+            value = cur_count
+            
+            break
 
+        for word in words:            
+            duplicate = 0
+            for index, char in enumerate(word):
+            
+                # 2개 이상 다른 단어
+                if duplicate == 2:
+                    break
+            
+                # 문자 다른 단어 검사
+                if char != cur_word[index]:
+                    duplicate += 1
+            
+                if index == len(word)-1 and duplicate == 1:
+                    # 큐에 넣기
+                    transform = Tansform(cur_count+1, word, words)
+                    queue.put(transform)
+                    words.remove(word)
+                     
     
-def solution(maps):
-    max_x = len(maps[0])
-    max_y = len(maps)
+    return value
+        
     
-    bfs(max_x, max_y, maps)
     
-    if len(answer) > 0:
-        return min(answer)
-    return -1
+def solution(begin, target, words):       
+    return bfs(begin, target, words)
