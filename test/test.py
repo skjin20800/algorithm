7b1c6095056e3b7a87a913d87da7574e01e633ec
kyoungbin <skjin20800@naver.com>
2024-03-06 18:28:47 +0900

    거 리 두 기 확 인 하 기

--- test/test.py
+++ test/test.py
@@ -1,67 +1,112 @@
-import copy
+def right(x,ary):
+    
+    try:
+        if x == 4:
+            return True
+        elif ary[x+1] == "X"  :
+            return True
+        elif ary[x+1] == "P":
+            return False
+        elif x == 3:
+            return True
+        elif ary[x+2] == "X":
+            return True
+        elif ary[x+2] == "P":
+            return False
+        return True
+    except Exception as e:
+        print(e)
+        return True
+        
 
-def solution(tickets):
+def bottom(x,y,place):
+    
+    try:
+     #   print(place,y,x)
+
+        if y == 4:
+            return True
+        elif place[y+1][x] == "X":
+            return True
+        elif place[y+1][x] == "P":
+            return False
+        if y == 3:
+            return True
+        elif place[y+2][x] == "X":
+            return True
+        elif place[y+2][x] == "P":
+            return False
+        return True
+    except Exception as e:
+        print(e)
+        return True
+        
+def bottom_right(x,y,place):
+    try:    
+        #우 하단 
+        if place[y][x+1] == "O" and place[y+1][x+1] == "P":
+            if x == 4:
+                return True
+            return False
+        
+        if place[y+1][x] == "O" and place[y+1][x+1] == "P":
+            if x == 4:
+                return True
+            return False
+        return True
+    except Exception as e:
+        print(e)
+        return True
+def bottom_left(x,y,place):
+    try:    
+        # 좌 하단 
+        if place[y][x-1] == "O" and place[y+1][x-1] == "P":
+            if x  == 0:
+                return True
+            return False
+        
+        if place[y+1][x] == "O" and place[y+1][x-1] == "P":
+            if x  == 0:
+                return True
+            return False
+        
+
+        return True
+    except Exception as e:
+        print(e)
+        return True
+        
+    
+    
+    
+
+def default(place):
+    is_right = True
+    is_bottom = True
+    is_bottom_right = True
+    is_bottom_left = True
+    for y, ary in enumerate(place):
+        for x, P in enumerate(ary):
+            if P == "P":
+                print("우",right(x,ary))
+                is_right = right(x,ary)
+                print("바",bottom(x,y,place))
+                is_bottom = bottom(x,y,place)
+                print("대우",bottom_right(x,y,place))
+                is_bottom_right = bottom_right(x,y,place)
+                print("대좌",bottom_left(x,y,place))
+                is_bottom_left = bottom_left(x,y,place)
+                
+                print(bottom_right(x,y,place), place, x, y)
+            if is_right == False or is_bottom == False or is_bottom_right == False or is_bottom_left == False: 
+                return 0
+    return 1
+        
+    
+
+def solution(places):
     answer = []
-    ticket_dic = {}
-    result_dict = []
-    
-    for ticket in tickets:
-        if ticket[0] in ticket_dic:
-            ticket_dic[ticket[0]].append(ticket[1])
-            ticket_dic[ticket[0]].sort()
-        else:
-            ticket_dic[ticket[0]] = [ticket[1]]
-
-    max_count = len(tickets) +1
-    
-    
-    def dfs(start,ticket_dic, result):
-        try:
-            if start not in ticket_dic:
-                result_dict.append(result)
-              #  print("끝2")
-                return
-            
-            if len(ticket_dic[start]) == 0:
-                result_dict.append(result)
-              #  print("끝")
-                return
-            
-            for ticket in ticket_dic[start]:                
-                dfs_result = copy.deepcopy(result)
-                dfs_dic = copy.deepcopy(ticket_dic)
-                # start : 시작
-                # ticket_dic[start] : 목적지 리스트
-                # ticket : 도착지
-                
-                # 현재 시작은 동일
-                # 도착지에따라 DFS 가동 
-                
-                
-                # 도착지 저장                
-                dfs_result.append(ticket)
-               
-                # 도착지 삭제                 
-                dfs_dic[start].remove(ticket)
-                
-                # 출발 
-                dfs(ticket,dfs_dic,dfs_result)
-            
-            
-        except Exception as e:
-          #  print("에러발생")
-            print(e)
-
-    #esult.append(tickets[0][0])
-    #result.append(tickets[0][0])
-
-    
-
-    dfs_result = []
-    dfs_result.append("ICN")
-    dfs("ICN", ticket_dic,dfs_result)
+    for place in places:
+        answer.append(default(place))
         
-    for item in result_dict:
-        if len(item) == max_count:
-            return item
-        
+    return answer
