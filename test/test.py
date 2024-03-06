34d83e868e941fa31f46c0247ecb0c5722b48d33
kyoungbin <skjin20800@naver.com>
2024-03-06 12:04:10 +0900

    기능개발

--- test/test.py
+++ test/test.py
@@ -1,44 +1,29 @@
-import heapq
-
-def pop(scoville, K, count):
-    if  scoville[0] < K and len(scoville) > 1:
-        one = heapq.heappop(scoville)
-        two = heapq.heappop(scoville)
-        three = one + (two *2)
-        heapq.heappush(scoville, three)
-        count += 1    
-        count = pop(scoville, K, count)
-    elif len(scoville) == 1 and scoville[0] < K:
-        return -1
-    elif len(scoville) == 1 and scoville[0] > K:
-        return count
-    else :
-        return count
-    return count
+def solution(progresses, speeds):
+    answer = []
     
-
-def solution(scoville, K):
-    heapq.heapify(scoville)
-    if scoville[0] > K:
-        return -1
+    day = 0
+    result = []
+    for item1, speed in zip(progresses, speeds):
+        
+        pg_day = day
+        item1 = item1 + (pg_day * speed)
+        while item1 < 100:
+            item1 += speed
+            day += 1
+        
+        result.append(day)
     
-    count = 0
-    result = 0
-    while True:
-        if  scoville[0] < K and len(scoville) > 1:            
-            one = heapq.heappop(scoville)
-            two = heapq.heappop(scoville)
-            three = one + (two *2)
-            heapq.heappush(scoville, three)
-            count += 1    
-        elif len(scoville) == 1 and scoville[0] < K:
-            result = -1
-            break
-        elif len(scoville) == 1 and scoville[0] > K:
-            result = count
-            break
-        else :
-            result = count
-            break
-
-    return result
+    result_dic = {}
+    for val in result:
+        if val in result_dic:
+            result_dic[val] += 1
+        else:            
+            result_dic[val] = 1
+    answer = list(result_dic.values())
+    
+    
+    #for val in result:
+        
+    
+    
+    return answer
