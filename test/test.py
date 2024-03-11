9150279f3d26e5f9c13ddd910534847d7b7c1f05
JinKyoungBin <74043683+skjin20800@users.noreply.github.com>
2024-03-11 18:47:53 +0900

    올바른 괄호

--- test/test.py
+++ test/test.py
@@ -1,47 +1,13 @@
-def sum_sharp(arr,sharp_arr,n):
-    for y in range(n):
-        for x in range(n):        
-            if len(sharp_arr) > 0 and y == sharp_arr[0][0] and x == sharp_arr[0][1]:
-                sharp_arr.pop(0)                
-                temp = list(arr[y])
-                temp[x] = "#"
-                arr[y] = "".join(temp)
-    return arr
+def solution(s):
+    temp_count = 0    
+    for item in s:
+        if item == "(":
+            temp_count += 1
+        else:
+            if temp_count <= 0:
+                return False
+            temp_count -= 1
 
-def check_sharp(arr,n):
-    sharp_arr = []
-    for y in range(n):
-        for x in range(n):
-            if arr[y][x] == "#":
-                sharp_arr.append([y,x])
-    return sharp_arr
-    
-
-def int_ary_to_binary_ary(arr,n):
-    result = []
-    for num in arr:
-        temp = format(num,"b")
-        temp2 = list(temp)
-        for x, num in enumerate(temp2):
-            if num == "1":
-                temp2[x] = "#"
-            else:
-                temp2[x] = " "
-        temp2 = "".join(temp2)
-        
-        while len(temp2) < n:
-            temp2 = " " + temp2
-        result.append(temp2)
-    return result
-        
-
-def solution(n, arr1, arr2):
-    answer = []
-    
-    map1 = int_ary_to_binary_ary(arr1,n)
-    map2 = int_ary_to_binary_ary(arr2,n)
-    sharp_arr = check_sharp(map1,n)
-    answer = sum_sharp(map2,sharp_arr,n)
-
-    
-    return answer
+    if temp_count != 0:
+        return False
+    return True
